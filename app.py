ac273bcd5fa43b78eda4a19886c364c56f5737bb
alejandro-orte <69557357+alejandro-orte@users.noreply.github.com>
2026-09-07 15:49:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ else:
         st.session_state["nombre"] = ""
         st.rerun()
 
-    st.title(f"Asociado {st.session_state['nombre']}")
+    st.title(f"Bienvenido {st.session_state['nombre']}")
     usuario_key = st.session_state["usuario"]
     
     try:
